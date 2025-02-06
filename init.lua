f10757526182d982a375b94a3de85b2df9bc9c82
Chihiro Haga <chihiro.haga@ge.see.eng.osaka-u.ac.jp>
2025-02-07 08:49:37 +0900

    add copilot and ayu-mirage

--- init.lua
+++ init.lua
@@ -229,6 +229,8 @@ vim.opt.rtp:prepend(lazypath)
 require('lazy').setup({
   -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
   'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
+  'github/copilot.vim', -- GitHub copilot, ch add
+  'Shatur/neovim-ayu', -- Ayu theme
 
   -- NOTE: Plugins can also be added by using a table,
   -- with the first argument being the link and the following
